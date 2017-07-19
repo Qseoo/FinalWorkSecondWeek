715cfcf93544ad7818485e7d958ba13c959a3cac
Qseoo <mikelis.gulbis@gmail.com>
2017-07-19 17:05:40 +0300

    Initial files n stuff

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# PicogramDay3
-Optional
+# picogram
+Picogram
---
+++ script.js
@@ -0,0 +1,8 @@
+'use strict';
+
+import './style.less';
+
+
+let div = document.getElementById('picogram')
+console.log(div);
+div.innerHTML = 'picogram';
